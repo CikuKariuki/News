0a7a71730252f8af646d5dfe3f7d7ff99a7e7577
Wanjiku <Wanjiku>
2019-04-15 18:39:15 +0300

    routed views to news.html successfully

--- app/views.py
+++ app/views.py
@@ -8,4 +8,12 @@ def index():
     '''
     function that returns the index page and its data
     '''
-    return render_template('index.html')
+    message = "News Highlights"
+    return render_template('index.html', message = message)
+
+@app.route('/news/<news_id>')
+def news(news_id):
+    '''
+    returns the news details page and its data
+    '''
+    return render_template('news.html', id=news_id)
