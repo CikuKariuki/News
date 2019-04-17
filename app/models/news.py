be0c22e3e8978483062184ee4a7fef9ae08dc25a
Wanjiku <Wanjiku>
2019-04-17 08:18:14 +0300

    obtained data from url

---
+++ app/models/news.py
@@ -0,0 +1,9 @@
+class News:
+    '''
+    class to define news objects
+    '''
+    def __init__(self,id,name,description,language):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.language = language
--- app/request.py
+++ app/request.py
@@ -1,4 +1,27 @@
 from app import app
+import urllib.request,json
+from .models import news
+
+News = news.News
 
 # Getting api key
 api_key = app.config['NEWS_API_KEY']
+
+#Getting the news base url
+base_url = app.config["NEWS_API_BASE_URL"]
+
+def get_news(sources):
+    '''
+    gets json response to url request
+    '''
+    get_news_url = base_url.format(api_key)
+    with urllib.request.urlopen(get_news_url) as url:
+        get_news_data = url.read() #to read response
+        get_news_response = json.loads(get_news_data)
+
+        news_results = None
+
+        if get_news_response['results']:
+            news_results_list = get_news_response['results']
+            news_results = process_results(news_results_list) #takes in a list of dictionary objects and returns a list of news objects
+    return news_results #list of news objects
