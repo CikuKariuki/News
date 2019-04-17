4958c8d1284035b8fb58c4946b94a52dbf84b285
Wanjiku <Wanjiku>
2019-04-17 07:23:51 +0300

    Confirmed test  has failed in news_test.py

---
+++ app/news_test.py
@@ -0,0 +1,19 @@
+import unittest
+from models import news
+News = news.news
+
+class NewsTest(unittest.TestCase):
+    '''
+    Test class to test the behaviour of the news class
+    '''
+    def setUp(self):
+        '''
+        set up method that will run before every test
+        '''
+        self.new_news = News("abc-news","ABC News","Your trusted source for breaking news","en")
+    
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_news,News))
+
+        if __name__ == '__main__':
+            unittest.main()
